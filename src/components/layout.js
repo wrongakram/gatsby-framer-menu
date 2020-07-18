1ccd02d7812e944d9b23cacc2e9c223ea7372cba
Akram Khalid <akramkhalid12@gmail.com>
2020-07-17 22:03:34 -0500

    Banner Header and acomplishments section created

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,19 +28,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   )
